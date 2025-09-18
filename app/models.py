f0397f61be25e1662b8c1b751c26f30a4f9d32b7
Ben Nagy <51839240+bjnagy@users.noreply.github.com>
2025-09-18 19:44:14 +0000

    add followers junction table

--- app/models.py
+++ app/models.py
@@ -11,6 +11,15 @@ from hashlib import md5
 def load_user(id):
     return db.session.get(User, int(id))
 
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True),
+    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True)
+)
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -24,6 +33,15 @@ class User(UserMixin, db.Model):
 
     events: so.WriteOnlyMapped['Event'] = so.relationship(
         back_populates='source')
+    
+    following: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates='followers')
+    followers: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates='following')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -37,6 +55,39 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery())
+        return db.session.scalar(query)
+
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_posts(self):
+        Source = so.aliased(User)
+        Follower = so.aliased(User)
+        return (
+            sa.select(Event)
+            .join(Event.source.of_type(Source))
+            .join(Source.followers.of_type(Follower))
+            .where(Follower.id == self.id)
+            .order_by(Event.timestamp.desc())
+        )
     
 class Event(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
